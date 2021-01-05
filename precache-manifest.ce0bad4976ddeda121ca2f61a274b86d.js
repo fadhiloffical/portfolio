b4abc431dcf912944d956ed826d37cacc61c45f4
fadhilasharaf1996 <fadhilasharaf@gmail.com>
2021-01-05 17:10:35 +0530

    Updates

---
+++ precache-manifest.ce0bad4976ddeda121ca2f61a274b86d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c2fe7a351ff8a47fb8dad215d5c531",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5a620b63581b96ee5581",
+    "url": "/portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "de502d186eb5281f176f",
+    "url": "/portfolio/static/css/main.a30a70b8.chunk.css"
+  },
+  {
+    "revision": "5a620b63581b96ee5581",
+    "url": "/portfolio/static/js/2.f756fe22.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.f756fe22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de502d186eb5281f176f",
+    "url": "/portfolio/static/js/main.30ffb6fb.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "6ff817ecb9d543089127e5d49f03deae",
+    "url": "/portfolio/static/media/BlenderPro-Book.6ff817ec.woff"
+  },
+  {
+    "revision": "f225752e396c47e5cb922054ac546204",
+    "url": "/portfolio/static/media/BlenderPro-Medium.f225752e.woff"
+  },
+  {
+    "revision": "1fb2e25bab43811bb10a05c11ef78503",
+    "url": "/portfolio/static/media/abstract.1fb2e25b.png"
+  },
+  {
+    "revision": "f16f30709ceae23a55235ad1a63019e3",
+    "url": "/portfolio/static/media/amad.f16f3070.jpg"
+  },
+  {
+    "revision": "2916bf13b430162d64f67eb642eff61b",
+    "url": "/portfolio/static/media/fingmygpartner.2916bf13.jpg"
+  },
+  {
+    "revision": "69c37adb20cc82a121d9544f30641e2b",
+    "url": "/portfolio/static/media/nimfra.69c37adb.jpg"
+  },
+  {
+    "revision": "fe059d6c7fda9a9c4d628f47fb88a291",
+    "url": "/portfolio/static/media/portfolio.fe059d6c.jpg"
+  },
+  {
+    "revision": "55455467ea4b13afbd821a104bb5d692",
+    "url": "/portfolio/static/media/timehut.55455467.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fadhil/precache-manifest.872238e59acd5024d0f33877dfa62f71.js"
+  "/portfolio/precache-manifest.ce0bad4976ddeda121ca2f61a274b86d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fadhil/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
